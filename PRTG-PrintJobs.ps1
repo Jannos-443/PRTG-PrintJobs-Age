04516f5592b87224e477ecbd40f6a9418572a80a
Jannos-443 <77551365+Jannos-443@users.noreply.github.com>
2022-02-28 08:47:10 +0100

    Update PRTG-PrintJobs.ps1

--- PRTG-PrintJobs.ps1
+++ PRTG-PrintJobs.ps1
@@ -21,7 +21,7 @@
      
       Example: ^(DT_IT_B10_P107, 238|TestPrinter123)$
 
-      Example2: ^(Test123.*|TestPrinter555)$ excluded Test12345 und alles mit 
+      Example2: ^(Test123.*|TestPrinter555)$ excludes TestPrinter555 and any Printer starting with Test123
 
     #https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_regular_expressions?view=powershell-7.1
 
@@ -32,10 +32,28 @@
     .PARAMETER Password
     Provide the Windows password for the user specified to connect to the target machine using WMI. Better way than explicit credentials is to set the PRTG sensor
     to launch the script in the security context that uses the "Windows credentials of parent device".
+    
+    .PARAMETER HttpPush
+    enables HTTP Push in the Sensor (requires HttpToken, HttpServer and HttpPort)
+
+    .PARAMETER HttpToken
+    Set your HTTP Push Sensor Token (Token is available in the Sensor Settings after creating the Sensor)
+
+    .PARAMETER HttpServer
+    Set the Target HTTP Push Server (YourPRTGServer FQDN)
+
+    .PARAMETER HttpPort
+    Use this parameter if you need to use a HTTP Push Port other than 5050
+
+    .PARAMETER HttpPushUseSSL
+    Use this parameter to set the HTTP Push to use HTTPS
 
     .EXAMPLE
     Sample call from PRTG EXE/Script Advanced
     PRTG-PrintJobs.ps1 -ComputerName %host -Age 5
+
+    Sample call from Task Scheduler on Remote Computer
+    -Command "& 'D:\Powershell\PRTG-PrintJobs.ps1' -ComputerName 'localhost' -HttpPush -HttpServer 'YourPRTGServer' -HttpPort '5050' -HttpToken 'YourHTTPPushToken'"
 
     .NOTES
     This script is based on the sample by Paessler (https://kb.paessler.com/en/topic/67869-auto-starting-services) and debold (https://github.com/debold/PRTG-WindowsServices)
@@ -46,14 +64,17 @@
 param(
     [string]$ComputerName = "",
     [string]$IgnorePattern = '',
-    [int]$Age = "1", # Jobs is older than x minutes
+    [int]$Age = "1",                # Jobs is older than x minutes
     [string]$UserName = "",
-    [string]$Password = ""
+    [string]$Password = "",
+    [switch] $HttpPush,             #enables http push, usefull if you want to run the Script on the target Server to reduce remote Permissions
+    [string] $HttpToken,            #http push token
+    [string] $HttpServer,           #http push prtg server hostname
+    [string] $HttpPort = "5050",    #http push port (default 5050)
+    [switch] $HttpPushUseSSL        #use https for http push
 )
 
-
 #Catch all unhandled Errors
-$ErrorActionPreference = "Stop"
 trap{
     if($session -ne $null)
         {
@@ -62,12 +83,16 @@ trap{
     $Output = "line:$($_.InvocationInfo.ScriptLineNumber.ToString()) char:$($_.InvocationInfo.OffsetInLine.ToString()) --- message: $($_.Exception.Message.ToString()) --- line: $($_.InvocationInfo.Line.ToString()) "
     $Output = $Output.Replace("<","")
     $Output = $Output.Replace(">","")
+    $Output = $Output.Replace("#","")
     Write-Output "<prtg>"
     Write-Output "<error>1</error>"
-    Write-Output "<text>$Output</text>"
+    Write-Output "<text>$($Output)</text>"
     Write-Output "</prtg>"
     Exit
 }
+
+# Error if there's anything going on
+$ErrorActionPreference = "Stop"
 
 
 if ($ComputerName -eq "") {
@@ -77,10 +102,6 @@ if ($ComputerName -eq "") {
     Write-Output "</prtg>"
     Exit
 }
-
-# Error if there's anything going on
-$ErrorActionPreference = "Stop"
-
 
 # Generate Credentials Object, if provided via parameter
 try{
@@ -107,7 +128,7 @@ $old = (Get-Date).AddMinutes(-$Age)
 # Get list of Jobs that are older than the Age.
 try 
     {
-    if ($Credentials -eq $null) 
+    if ($null -eq $Credentials) 
         {
         $PrintJobs = Get-CimInstance -Namespace "root\CIMV2" -ClassName $WmiClass -ComputerName $ComputerName | Where-Object {$_.timesubmitted -lt $old}
         } 
@@ -136,17 +157,17 @@ $IgnoreScript = '^(TestIgnore)$'
 #Remove Ignored Printer
 if ($IgnorePattern -ne "") 
     {
-    $PrintJobs = $PrintJobs | where {$_.Name -notmatch $IgnorePattern}  
+    $PrintJobs = $PrintJobs | Where-Object {$_.Name -notmatch $IgnorePattern}  
     }
 
 if ($IgnoreScript -ne "") 
     {
-    $PrintJobs = $PrintJobs | where {$_.Name -notmatch $IgnoreScript}  
+    $PrintJobs = $PrintJobs | Where-Object {$_.Name -notmatch $IgnoreScript}  
     }
 
 
 $Count = ($PrintJobs | Measure-Object).Count
-$Error = ""
+$ErrorText = ""
 
 $xmlOutput = '<prtg>'
 
@@ -161,27 +182,52 @@ if($Count -ge 1)
         else{
             $PName = $PrintJob.Name
             }
-        $Error += "Printer=$($PName) Owner=$($PrintJob.owner); "
+        $ErrorText += "Printer=$($PName) Owner=$($PrintJob.owner); "
           
         }
 
-    $xmlOutput = $xmlOutput + "<text>$($count) PrintJob(s) older $($age) min; $($Error)</text>"
+    $xmlOutput += "<text>$($count) PrintJob(s) older $($age) min; $($ErrorText)</text>"
     }
 
 else{
-    $xmlOutput = $xmlOutput + "<text>No PrintJobs older than $($Age) min.</text>"
+    $xmlOutput += "<text>No PrintJobs older than $($Age) min.</text>"
     }
 
 
-$xmlOutput = $xmlOutput + "<result>
+$xmlOutput += "<result>
         <channel>Jobs pending</channel>
         <value>$count</value>
         <unit>Count</unit>
         <limitmode>1</limitmode>
-        <LimitMaxError>1.5</LimitMaxError>
-        <LimitMaxWarning>0.1</LimitMaxWarning>
+        <LimitMaxError>1</LimitMaxError>
+        <LimitMaxWarning>0</LimitMaxWarning>
         </result>"
 
-$xmlOutput = $xmlOutput + "</prtg>"
+$xmlOutput += "</prtg>"
 
-$xmlOutput
+#region: Http Push
+if($httppush)
+    {
+    if($HttpPushUseSSL)
+        {$httppushssl = "https"}
+    else
+        {$httppushssl = "http"}
+
+    Add-Type -AssemblyName system.web
+
+    $Answer=Invoke-Webrequest -method "GET" -URI ("$($httppushssl)://$($httpserver):$($httpport)/$($httptoken)?content=$(([System.Web.HttpUtility]::UrlEncode($xmloutput)))") -usebasicparsing
+
+    if ($answer.Statuscode -ne 200)
+        {
+        Write-Output "<prtg>"
+        Write-Output "<error>1</error>"
+        Write-Output "<text>http push failed</text>"
+        Write-Output "</prtg>"
+        Exit
+        }
+    }
+#endregion
+
+#finish Script - Write Output
+
+Write-Output $xmlOutput
